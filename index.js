3aa50d704dbbf3a5c7f1c27499a3157dbf613166
jamesndungu <jimmindungu3@gmail.com>
2023-09-16 11:15:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "James",
+    streetAddress: 20100,
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, sex, male) {
+    const newEmployee = {...employee};
+    newEmployee[sex] = male;
+    return newEmployee;
+}
+
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, name, Ndungu) {
+    employee[name] = Ndungu;
+    return(employee);
+}
+
+
+function deleteFromEmployeeByKey(employee) {
+    const employeeUpdate = {...employee};
+    delete employeeUpdate.streetAddress;
+    delete employeeUpdate.name;
+    return (employeeUpdate);
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,) {
+    delete employee.streetAddress;
+    delete employee.name;
+    return (employee);
+}
